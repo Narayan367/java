c74f6b40da4ab0714ba45af996632f6fcd7a4383
Narayan <narayandhungel156@gmail.com>
2024-01-09 07:47:30 +0545

    variable

---
+++ variable.java
@@ -0,0 +1,25 @@
+public class VariableIllustration {
+    public static void main(String[] args) {
+        // Integer variable
+        int myInt = 10;
+
+        // Double variable
+        double myDouble = 5.75;
+
+        // Boolean variable
+        boolean myBoolean = true;
+
+        // Character variable
+        char myChar = 'A';
+
+        // String variable
+        String myString = "Hello, Java!";
+
+        // Output the values of variables
+        System.out.println("Integer variable: " + myInt);
+        System.out.println("Double variable: " + myDouble);
+        System.out.println("Boolean variable: " + myBoolean);
+        System.out.println("Character variable: " + myChar);
+        System.out.println("String variable: " + myString);
+    }
+}
