9cb1ac3c49def8f67db76735936f6246e3de0770
Narayan <narayandhungel156@gmail.com>
2024-01-09 07:28:06 +0545

    first commit

---
+++ first.java
@@ -0,0 +1,11 @@
+// public class Studen {
+//     public static void main( string args[]){
+//         System.out.printIn('Hello wrold ');
+//     }
+// }
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
